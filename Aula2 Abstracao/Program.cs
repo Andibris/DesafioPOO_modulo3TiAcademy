b8f9d6677d761eb937a51eaac121114f05ae1559
Anderson Brischiliari <88595771+Andibris@users.noreply.github.com>
2022-11-29 01:13:09 -0300

    Aula2

---
+++ Aula2 Abstracao/Program.cs
@@ -0,0 +1,15 @@
+﻿//Aula 1 foi teórica, explicando os pilares da programação orientada a objetos.
+//Aula 2 - Abstração
+using Aula2.Models;
+
+//Instanciação de um objeto da classe pessoa:
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Anderson";
+pessoa1.Idade = 32;
+pessoa1.Apresentar(); //método da classe pessoa
+
+//Instanciando outro objeto da classe pessoa:
+Pessoa pessoa2 = new Pessoa();
+pessoa2.Nome = "Elaine";
+pessoa2.Idade = 33;
+pessoa2.Apresentar();
