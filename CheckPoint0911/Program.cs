6a6f3a30996b4b13cddf1f47177cc3ac91b2bf06
Anderson Brischiliari <88595771+Andibris@users.noreply.github.com>
2022-11-29 01:16:53 -0300

    Checkpoint09-11-2022

---
+++ CheckPoint0911/Program.cs
@@ -0,0 +1,25 @@
+﻿//do while
+
+//FOR
+//int numero = 2; //posso escolher o número que eu quiser.
+
+//for(int i = 0; i < 10; i++) //para cada contador setado em zero, enquanto for menor que 10, incremento do contador em 1 unidade. 10 = valor arbitrário à minha escolha.
+//{
+//    Console.WriteLine($"{numero} X {i} = {numero * i}");
+//}
+
+
+//WHILE
+//int acumulador = 0;
+//int numero = 1;
+
+//while(numero != 0)
+//{
+//    Console.WriteLine("Digite o valor numérico:");
+//    numero = Convert.ToInt32(Console.ReadLine());
+//    acumulador = acumulador + numero;    
+//}
+//Console.WriteLine($"A soma dos números é: {acumulador}");
+
+//DO WHILE - explanado em 11/11/2022
+
